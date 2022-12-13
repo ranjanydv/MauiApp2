eeb36e411a11da3658d1931991fce6d37c42a1eb
Ranjan Yadav <theranzanydv@gmail.com>
2022-12-13 11:40:41 +0545

    Add project files.

---
+++ MauiApp2/Data/EmployeeService.cs
@@ -0,0 +1,31 @@
+﻿using MauiApp2.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp2.Data
+{
+    public class EmployeeService
+    {
+        public List<EmployeeModel> EmployeeDetails()
+        {
+            List<EmployeeModel> employeeList = new List<EmployeeModel>()
+            {
+                new EmployeeModel(){FirstName="Ranjan",LastName="Yadav",Designation="Manager",StartDate=DateTime.Now,Salary=120000},
+                new EmployeeModel(){FirstName="Samiksha",LastName="Bhandari",Designation="CEO",StartDate=DateTime.Now,Salary=240000},
+                new EmployeeModel(){FirstName="Apple",LastName="Ball",Designation="Cat",StartDate=DateTime.Parse("10/04/2021"),Salary=120000},
+            };
+            return employeeList;
+        }
+
+        public List<EmployeeModel> GetEmployeeData(string name)
+        {
+            var employeeList = EmployeeDetails();
+            var designation = employeeList.Where(x => x.FirstName.Contains(name, StringComparison.OrdinalIgnoreCase)).ToList();
+            return designation;
+        }
+
+    }
+}
